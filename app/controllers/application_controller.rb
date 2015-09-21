9736b98a3f5dae4f2bcaa15b24f7985e35830f54
SureshMenon37 <mysticmiz@gmail.com>
2015-09-21 13:35:55 +0530

    Done with static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  <title>Ruby on Rails Tutorial Sample App | <%= @title %></title>
+<%= csrf_meta_tag %>
 end
